e78ffb6f0dfc7d217a9eccac32bdfa314d713c23
Jeremias Aitor <oyhamburo.jeremias@gmail.com>
2022-10-11 21:28:12 -0300

    desafio 4

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const multer = require("multer");
+const PORT = 8080
+const server = app.listen(PORT, ()=> console.log(`server listen on PORT ${PORT}`))
+server.on('error',err => console.log(`Error: ${err}`))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/uploads'))
+
+
+// CONFIGURADO MULTER
+const myStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb ) =>{
+        const filename = `${Date.now()}-${file.originalname}`
+        cb(null,filename)
+    }
+})
+// EJECUCION PARA TABAJAR LOCALMENTE EL MULTER
+const upload = multer({storage: myStorage})
+
+
+app.post('/uploadfile', upload.single('myFile'),(req,res)=>{
+    const file = req.file
+    console.log(file)
+    if(!file){
+        return res.status(404).send('Error subiendo archivos')
+    }
+    res.status(200).json({
+        status:'Archivo subido correctamente! ',
+        link:__dirname + '/uploads/'+file.filename
+    })
+})
+
+
+
+
+
+const routerProductos = require('./routes/productos.js')
+app.use('/api/productos',routerProductos)
